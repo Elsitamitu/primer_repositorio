11e34b78a362a75f3b3ac8e331a9d3371cc07c67
Elsitamitu <abuelitamitu@gmail.com>
2025-03-28 17:29:37 -0300

    Se creò una rama llamada ejercicio_4rama1

--- Punto4.py
+++ Punto4.py
@@ -1,7 +1,7 @@
 # Crear un programa que pida al usuario el radio de un círculo e imprima por pantalla su área y 
 #su perímetro.
 pi=3.14159
-radio=float(input("Ingrese el radio del círculo :"))
+radio=float(input("Ingresar el radio del círculo :"))
 perímetro=float(2*pi*radio)
 área=float(pi*radio**2)
 print("El área del circulo es:",área,"y su perímetro es:",perímetro)
